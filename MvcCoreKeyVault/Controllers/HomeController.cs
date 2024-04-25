3bd1be62d231389fd9d98734e13a11b095283e24
anitapm03 <apereiramolinero@gmail.com>
2024-04-25 13:01:40 +0200

    Add project files.

---
+++ MvcCoreKeyVault/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Azure.Security.KeyVault.Secrets;
+using Microsoft.AspNetCore.Mvc;
+using MvcCoreKeyVault.Models;
+using System.Diagnostics;
+
+namespace MvcCoreKeyVault.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private SecretClient secretClient;
+
+
+        public HomeController(ILogger<HomeController> logger,
+            SecretClient secretClient)
+        {
+            this.secretClient = secretClient;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(string secretkey)
+        {
+            KeyVaultSecret secret = await
+                this.secretClient.GetSecretAsync(secretkey);
+            ViewData["SECRETO"] = secret.Value;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
